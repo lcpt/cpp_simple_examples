d0c10189ad91a7fd4b19c42da3139ca1751ef730
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-02-20 12:57:37 +0100

    Add 3D Fast Intersection and Distance Computation example

--- cgal_related/compile.sh
+++ cgal_related/compile.sh
@@ -1,2 +1,3 @@
-g++ segment_intersection.cc -lgmp -frounding-math -o segment_intersection
-g++ segments_to_polyline.cc -lgmp -lmpfr -frounding-math -o segments_to_polyline
+#g++ segment_intersection.cc -lgmp -frounding-math -o segment_intersection
+#g++ segments_to_polyline.cc -lgmp -lmpfr -frounding-math -o segments_to_polyline
+g++ AABB_triangle_3_example.cpp -lgmp -lmpfr -frounding-math -o AABB_triangle_3_example
