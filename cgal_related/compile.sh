f831f6b81b1f6be0cf13a47f6ff990cf876cb64d
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-02-22 11:25:18 +0100

    Update gitignore file

--- cgal_related/compile.sh
+++ cgal_related/compile.sh
@@ -1,3 +1,5 @@
 #g++ segment_intersection.cc -lgmp -frounding-math -o segment_intersection
 #g++ segments_to_polyline.cc -lgmp -lmpfr -frounding-math -o segments_to_polyline
-g++ AABB_triangle_3_example.cpp -lgmp -lmpfr -frounding-math -o AABB_triangle_3_example
+#g++ AABB_triangle_3_example.cpp -lgmp -lmpfr -frounding-math -o AABB_triangle_3_example
+#g++ box_d_do_intersect_polylines.cpp -lgmp -lmpfr -frounding-math -o box_d_do_intersect_polylines
+g++ AABB_polyhedron_facet_distance_example.cpp -lgmp -lmpfr -frounding-math -o AABB_polyhedron_facet_distance_example
