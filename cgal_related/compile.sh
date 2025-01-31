2ec2eaca9f1864972d19dcd540ee65017e78fbfe
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-01-31 16:02:56 +0100

    Add CGAL examples for enclosing rectangle and strip

--- cgal_related/compile.sh
+++ cgal_related/compile.sh
@@ -1,5 +1,7 @@
-#g++ segment_intersection.cc -lgmp -frounding-math -o segment_intersection
-#g++ segments_to_polyline.cc -lgmp -lmpfr -frounding-math -o segments_to_polyline
-#g++ AABB_triangle_3_example.cpp -lgmp -lmpfr -frounding-math -o AABB_triangle_3_example
-#g++ box_d_do_intersect_polylines.cpp -lgmp -lmpfr -frounding-math -o box_d_do_intersect_polylines
-g++ AABB_polyhedron_facet_distance_example.cpp -lgmp -lmpfr -frounding-math -o AABB_polyhedron_facet_distance_example
+# g++ segment_intersection.cc -lgmp -frounding-math -o segment_intersection
+# g++ segments_to_polyline.cc -lgmp -lmpfr -frounding-math -o segments_to_polyline
+# g++ AABB_triangle_3_example.cpp -lgmp -lmpfr -frounding-math -o AABB_triangle_3_example
+# g++ box_d_do_intersect_polylines.cpp -lgmp -lmpfr -frounding-math -o box_d_do_intersect_polylines
+# g++ AABB_polyhedron_facet_distance_example.cpp -lgmp -lmpfr -frounding-math -o AABB_polyhedron_facet_distance_example
+# g++ minimum_enclosing_rectangle_2.cpp -lgmp -lmpfr -frounding-math -o minimum_enclosing_rectangle_2
+g++ minimum_enclosing_strip_2.cpp -lgmp -lmpfr -frounding-math -o minimum_enclosing_strip_2
