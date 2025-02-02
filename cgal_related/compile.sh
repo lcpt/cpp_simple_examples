f5fcbe9a5a78f246812790444a0b373f5e4c23c6
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-02-02 16:48:38 +0100

    Add hexahedron example

--- cgal_related/compile.sh
+++ cgal_related/compile.sh
@@ -4,4 +4,5 @@
 # g++ box_d_do_intersect_polylines.cpp -lgmp -lmpfr -frounding-math -o box_d_do_intersect_polylines
 # g++ AABB_polyhedron_facet_distance_example.cpp -lgmp -lmpfr -frounding-math -o AABB_polyhedron_facet_distance_example
 # g++ minimum_enclosing_rectangle_2.cpp -lgmp -lmpfr -frounding-math -o minimum_enclosing_rectangle_2
-g++ minimum_enclosing_strip_2.cpp -lgmp -lmpfr -frounding-math -o minimum_enclosing_strip_2
+# g++ minimum_enclosing_strip_2.cpp -lgmp -lmpfr -frounding-math -o minimum_enclosing_strip_2
+g++ polyhedron_prog_cube.cpp -lgmp -lmpfr -frounding-math -o polyhedron_prog_cube
