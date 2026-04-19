ea9e8790d72799a04ac6a1017b4d0c820b82f6e3
Luis Claudio Pérez Tato <l.pereztato@ciccp.es>
2026-04-19 18:46:46 +0200

    Add variadic function example

---
+++ variadic_function_arguments/variadic_function_example_01.cc
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void simple_printf(const char* fmt, ...)
+{
+    va_list args;
+
+    for (va_start(args, fmt); *fmt != '\0'; ++fmt)
+    {
+        switch(*fmt)
+        {
+            case 'd':
+            {
+                int i = va_arg(args, int);
+                printf("%d\n", i);
+                break;
+            }
+            case 'c':
+            {
+                // A 'char' variable will be promoted to 'int'
+                // A character literal in C is already 'int' by itself
+                int c = va_arg(args, int);
+                printf("%c\n", c);
+                break;
+            }
+            case 'f':
+            {
+                double d = va_arg(args, double);
+                printf("%f\n", d);
+                break;
+            }
+            default:
+                puts("Unknown formatter!");
+                goto END;
+        }
+    }
+END:
+    va_end(args);
+}
+
+int main(void)
+  {
+    simple_printf("dcff", 3, 'a', 1.969, 42.5);
+  }
