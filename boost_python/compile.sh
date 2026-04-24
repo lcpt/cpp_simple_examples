c270d25bbd0d1628bc6b810b33ea6c548426ed36
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2026-04-24 11:10:37 +0200

    Add boost.python.numpy example

--- boost_python/compile.sh
+++ boost_python/compile.sh
@@ -1,1 +1,3 @@
 g++ -O2 -shared -fPIC example.cpp -o example.so -I/usr/include/python3.10 -lboost_python310
+
+g++ -Wall -Wextra -fPIC -shared -I/usr/include/python3.10/ numpy_example.cc -o numpy_example.so -lboost_python310 -lboost_numpy310
